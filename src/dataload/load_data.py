50add7afe32c1c5da16d364a1fae6d1094f2064c
minsoo9506 <ghktjd15gh@gmail.com>
2022-09-26 23:54:14 +0900

    format: formatting

--- src/dataload/load_data.py
+++ src/dataload/load_data.py
@@ -1,8 +1,7 @@
-from msilib.schema import Class
-import os
 from typing import Tuple
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 

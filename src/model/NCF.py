e35510728c441c9b0f4eb399416cfcfdc872ebdc
minsoo9506 <ghktjd15gh@gmail.com>
2022-09-26 23:54:36 +0900

    feat: NCF 모델 def __init__ 구현

--- src/model/NCF.py
+++ src/model/NCF.py
@@ -1,4 +1,24 @@
-from torch import nn, optim
+from typing import List
+
 import torch
 import torch.nn.functional as F
-from torch.autograd import Variable
+from torch import nn, optim
+
+
+class NeuralMatrixFactorization(nn.Module):
+    def __init__(
+        self,
+        num_users: int,
+        num_items: int,
+        gmf_emb_dim: int,
+        mlp_emb_dim: int,
+        mlp_hidden_dims_list: List[int],
+    ):
+        super().__init__()
+        self.gmf_user_emb = nn.Embedding(num_users, gmf_emb_dim)
+        self.gmf_item_emb = nn.Embedding(num_items, gmf_emb_dim)
+        self.mlp_user_emb = nn.Embedding(num_users, mlp_emb_dim)
+        self.mlp_item_emb = nn.Embedding(num_items, mlp_emb_dim)
+
+        # MLP layer들 생성 구현
+        # 마지막 NeuMF layer 구현

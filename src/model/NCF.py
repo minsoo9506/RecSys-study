8d0c8e7eeae9a3a471f856062e1230f3ed50ad14
minsoo9506 <ghktjd15gh@gmail.com>
2022-09-27 23:30:59 +0900

    feat: forward 함수 구현

--- src/model/NCF.py
+++ src/model/NCF.py
@@ -1,8 +1,7 @@
 from typing import List
 
 import torch
-import torch.nn.functional as F
-from torch import nn, optim
+import torch.nn as nn
 
 
 class NeuralMatrixFactorization(nn.Module):
@@ -14,11 +13,61 @@ class NeuralMatrixFactorization(nn.Module):
         mlp_emb_dim: int,
         mlp_hidden_dims_list: List[int],
     ):
+        """_summary_
+
+        Parameters
+        ----------
+        num_users : int
+            num of unique users
+        num_items : int
+            num of unique items
+        gmf_emb_dim : int
+            embedding dimensions of GMF
+        mlp_emb_dim : int
+            embedding dimensions of MLP
+        mlp_hidden_dims_list : List[int]
+            list of MLP hidden dimensions
+        """
         super().__init__()
         self.gmf_user_emb = nn.Embedding(num_users, gmf_emb_dim)
         self.gmf_item_emb = nn.Embedding(num_items, gmf_emb_dim)
         self.mlp_user_emb = nn.Embedding(num_users, mlp_emb_dim)
         self.mlp_item_emb = nn.Embedding(num_items, mlp_emb_dim)
 
-        # MLP layer들 생성 구현
-        # 마지막 NeuMF layer 구현
+        mlp = [nn.Linear(2 * mlp_emb_dim, mlp_hidden_dims_list[0]), nn.ReLU()]
+        for i in range(1, len(mlp_hidden_dims_list) - 1):
+            mlp.append(nn.Linear(mlp_hidden_dims_list[i], mlp_hidden_dims_list[i + 1]))
+            mlp.append(nn.ReLU())
+        mlp.append(
+            nn.Linear(mlp_hidden_dims_list[-2], mlp_hidden_dims_list[-1])
+        )  # 마지막에는 RELU 없이
+        self.mlp = nn.Sequential(*mlp)
+
+        self.NeuMF_layer = nn.Linear(gmf_emb_dim + mlp_hidden_dims_list[-1], 1)
+
+    def forward(self, users: torch.Tensor, items: torch.Tensor) -> torch.Tensor:
+        """_summary_
+
+        Parameters
+        ----------
+        users : torch.Tensor
+            index of user
+        items : torch.Tensor
+            index of item
+
+        Returns
+        -------
+        torch.Tensor
+            predicted ratings (0 ~ 1)
+        """
+        element_wise_prod = torch.mul(
+            self.gmf_user_emb(users), self.gmf_item_emb(items)
+        )
+        mlp_concat = torch.cat(
+            self.mlp_user_emb(users), self.mlp_item_emb(items), dim=1
+        )
+        mlp_out = self.mlp(mlp_concat)
+        final_layer_input = torch.cat(element_wise_prod, mlp_out, dim=1)
+        out = self.NeuMF_layer(final_layer_input)
+        out = nn.Sigmoid(out)
+        return out

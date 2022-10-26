f5be6fca995bb2548f1f507260764ca341644eb5
minsoo9506 <ghktjd15gh@gmail.com>
2022-10-27 00:17:13 +0900

    feat: predict 부분 구현

--- src/model/FM.py
+++ src/model/FM.py
@@ -6,8 +6,31 @@ class FactorizationMachine:
         pass
         # weight들
 
-    def _predict(self):
-        pass
+    def _predict(self, X, w0, w, v, n_factors, i):
+        data = X.data
+        indices = X.indices
+        indptr = X.indptr
+        summed = np.zeros(n_factors)
+        summed_squared = np.zeros(n_factors)
+
+        # bias
+        pred = w0
+        # linear: w * x
+        for idx in range(indptr[i], indptr[i + 1]):
+            col_loc = indices[idx]
+            pred += w[col_loc] * data[col_loc]
+
+        # factor
+        for factor in range(n_factors):
+            for idx in range(indptr[i], indptr[i + 1]):
+                col_loc = indices[idx]
+                term = v[factor, col_loc] * data[idx]
+                summed[factor] += term
+                summed_squared[factor] += term**2
+
+            pred += 0.5 * (summed[factor] ** 2 - summed_squared[factor])
+
+        return pred, summed
 
     def _sgd(self):
         pass
